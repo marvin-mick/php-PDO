bfdc230af7d017c2cf59b23c72754a1ac4885f31
Marvin Mick <104308189+marvin-mick@users.noreply.github.com>
2022-04-24 17:25:28 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,8 +5,6 @@
   header('Location: dashboard.php');
   }
 
-  $title = "Connexion";
-
   if(!empty($_POST)){
     $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
     extract($post);
