b20ff47b042920afd1e37006e2e251e466e3f518
Marvin Mick <104308189+marvin-mick@users.noreply.github.com>
2022-04-24 16:05:13 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php require('db.php'); ?>
+
+<?php
+  if(!empty($_SESSION['user'])){
+  header('Location: dashboard');
+  }
+
+  $title = "Connexion";
+
+  if(!empty($_POST)){
+    $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    extract($post);
+    $errors = [];
+
+    if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+      array_push($errors, "L'adresse email n'est pas valide.");
+    }
+
+    if(empty($password)){
+      array_push($errors, "Le mot de passe est requis.");
+    }
+
+    if(empty($errors)){
+      $req = $db->prepare('SELECT * FROM users WHERE email = :email');
+      $req->bindValue(':email', $email, PDO::PARAM_STR);
+      $req->execute();
+
+      $user = $req->fetch();
+      if($user && password_verify($password, $user->password)){
+        $_SESSION['user'] = $user;
+        header('Location: dashboard');
+      }
+
+      array_push($errors, "Mauvais identifiants.");
+    }
+  }
+?>
