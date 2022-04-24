969cc93da5c26d0cc415ae9e64258a0c29665a82
Marvin Mick <104308189+marvin-mick@users.noreply.github.com>
2022-04-24 16:20:57 +0200

    Update delete-user.php

--- delete-user.php
+++ delete-user.php
@@ -2,24 +2,10 @@
 
 <?php
   if(empty($_SESSION['user'])){
-  header('Location: login');
+  header('Location: login.php');
   }
 
   $user = $_SESSION['user'];
-  $filePath = 'photos/'.$user->id.'/'.$user->photo;
-
-  $dir = new DirectoryIterator(dirname('photos/'.$user->id));
-
-  foreach ($dir as $fileinfo){
-    if($fileinfo->isDot()){
-      unlink($fileinfo->getPathname());
-    }
-  }
-
-  if($user->photo && file_exists($filePath) && is_file($filePath)){
-    unlink($filePath);
-    rmdir('photos/'.$user->id);
-  }
 
   $req = $db->prepare('DELETE FROM users WHERE id = :id');
   $req->bindValue(':id', $user->id, PDO::PARAM_INT);
@@ -27,5 +13,5 @@
 
   unset($_SESSION['user']);
   session_destroy();
-  header('Location: inscription');
+  header('Location: inscription.php');
 ?>
